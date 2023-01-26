3ecdabf638e0d4ed799e0bfe3f803a178053a60f
Sagnik Basak <110663497+SagnikBasak@users.noreply.github.com>
2023-01-26 13:52:11 +0530

    Add files via upload

---
+++ pattern6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the line no.: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n - 1; j >= i; j--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= 1; j++)
+        {
+            printf("   ");
+        }
+        for (int j = n; j >= i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
